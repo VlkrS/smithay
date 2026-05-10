c3fd033e0d9029611af5d85fc6ea9b60fe797252
GH <47375452+VlkrS@users.noreply.github.com>
2026-05-10 18:36:16 +0200

    OpenBSD fixes from tobhe@

--- src/backend/egl/ffi.rs
+++ src/backend/egl/ffi.rs
@@ -144,8 +144,12 @@ pub mod egl {
     use libloading::Library;
     use std::sync::{LazyLock, Once};
 
+    #[cfg(not(target_os = "openbsd"))]
     pub static LIB: LazyLock<Library> =
         LazyLock::new(|| unsafe { Library::new("libEGL.so.1") }.expect("Failed to load LibEGL"));
+    #[cfg(target_os = "openbsd")]
+    pub static LIB: LazyLock<Library> =
+        LazyLock::new(|| unsafe { Library::new("libEGL.so") }.expect("Failed to load LibEGL"));
 
     pub static LOAD: Once = Once::new();
     pub static DEBUG: Once = Once::new();
